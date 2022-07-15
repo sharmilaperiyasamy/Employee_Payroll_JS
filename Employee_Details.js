679d6a8be50373e4293db5e55e7a7a6bf1889b6e
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-15 22:50:58 +0530

    [Sharmila] Calculating monthly wage

--- Employee_Details.js
+++ Employee_Details.js
@@ -1,14 +1,8 @@
-const is_Present = 1;
-
-// uc1 Checking Attendance
-let Attendance = Math.floor(Math.random() * 10) % 2;
-if (Attendance == is_Present)
-{
-   console.log("Employee is Present");
-   let empHrs;
-   const wagePerHr = 20;
+let empHrs;
+const working_dayspermonth = 20;
+const wagePerHr = 20;
    // Calculating Daily wage of the employee
-   let working_Hrs = (Math.floor(Math.random() * 10) % 2) + 1;
+   //creating function
    function getWorkingHrs(working_Hrs)
    {
    switch (working_Hrs)
@@ -17,15 +11,18 @@ if (Attendance == is_Present)
     return 8;
     case 2:
        return 4;
+       default :
+       return 0;
    }
 }
+let totalEmpHrs = 0;
+console.log("----------Employee Wage Calculator----------");
+for (let day = 0; day < working_dayspermonth; day++)
+{
+    let working_Hrs = (Math.floor(Math.random() * 10) % 2) + 1;
    empHrs = getWorkingHrs(working_Hrs);
+   totalEmpHrs = totalEmpHrs + empHrs;
+}
    let emp_Monthly_wage = wagePerHr * empHrs;
    console.log("Working Hour of the Employee : " + empHrs);
    console.log("Employee's Daily wage : " + emp_Monthly_wage);
-
-}
-else
-{
-    console.log("Employee is Absent");
-}
